9b950baf9d19f6eef78995abe959d6334a5491f6
sranucci <sranucci@itba.edu.ar>
2023-06-13 22:25:43 -0300

    ADD: login parser function

--- src/mp3p/mp3pBuffer.c
+++ src/mp3p/mp3pBuffer.c
@@ -1,16 +1,55 @@
 #include "mp3pBuffer.h"
 
 
-mp3p_states parse_login_character(char c,mp3p_states state){
-    return 0;
+
+static mp3p_states parse_login_character(char c,mp3p_states state){
+    if (state == WAITFIRSTLINE){
+        if ( c == '\n')
+            return WAITLETTERCOMMAND;
+        return WAITFIRSTLINE;
+    }
+
+    if (state == WAITLETTERCOMMAND){
+        if ( c == 'u' || c == 'U')
+            return WAITSPACE;
+    }
+
+    if (state == WAITSPACE){
+        if ( c == ' ')
+            return WAITUSERNAME;
+        return INVALIDCOMMAND;
+    }
+
+    if ( state == WAITPASSWORD ){
+        if ( c == ' ')
+            return WAITSECONDSPACE;
+        return WAITPASSWORD;
+    }
+
+    if ( state == WAITSECONDSPACE){
+        if ( c == ' ')
+            return WAITPASSWORD;
+        return INVALIDCOMMAND;
+    }
+
+    if (state == WAITPASSWORD){
+        if ( c == '\0')
+            return COMMANDCOMPLETED;
+        return WAITPASSWORD;
+    }
+
+    return INVALIDCOMMAND;
 }
 
-mp3p_states login_parser(mp3p_data* data){
-    for ( int i = 0; i < data->inputBuffer.length ;i++){
-        char c = data->inputBuffer.bufferData[i];
-        
-        data->state = parse_login_character(c,data->state);
+mp3p_states login_parser(mp3p_buffer* buff){
+
+    for ( int i = buff->currentPosition; i < buff->length ;i++){
+        char c = buff->bufferData[i];    
+        buff->state = parse_login_character(c,buff->state);
     }
+
+
+    return buff->state;
 }
 
 
@@ -18,8 +57,9 @@ mp3p_states login_parser(mp3p_data* data){
 
 
 void initialize_mp3p_data(mp3p_data * data){
-    reset_mp3p_buffer(data);
+    reset_mp3p_buffer(&data->inputBuffer);
     data->parseStrategy = login_parser;
+    data->inputBuffer.state = WAITFIRSTLINE;
 }
 
 
